375d550d80ef2dec96220cdee237f51feb45d93b
SKim <Simonsoohyunkim@gmail.com>
2020-06-15 20:23:27 -0500

    First commit on desktop pc, begun converting pawn and move component to cpp

--- Source/Modelcule/MyCPP/MyCPPPawn.cpp
+++ Source/Modelcule/MyCPP/MyCPPPawn.cpp
@@ -1,13 +1,55 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MyCPPPawn.h"
+
 
 // Sets default values
 AMyCPPPawn::AMyCPPPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//Components
+	MainSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("MainSceneComponent"));
+	RootComponent = MainSceneComponent;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera -> SetupAttachment(RootComponent);
+	
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow->SetupAttachment(Camera);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh -> SetupAttachment(RootComponent);
+	
+	Collision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collision"));
+	Collision->SetupAttachment(Mesh);
+
+	GrabLocation = CreateDefaultSubobject<USceneComponent>(TEXT("GrabLocation"));
+	GrabLocation->SetupAttachment(MainSceneComponent);
+	
+	//Variables
+	IsGrabbing = false;
+	IsOpenUI = false;
+
+	//StaticMesh'/Engine/BasicShapes/Cylinder.Cylinder'
+
+}
+
+//Functions
+void AMyCPPPawn::GrabObject() 
+{
+	
+}
+
+AActor* AMyCPPPawn::FindRootObj(AActor* currObj)
+{
+
+	return currObj;
+}
+
+void AMyCPPPawn::RotateGrabbedObj(FRotator rot)
+{
 
 }
 
@@ -26,9 +68,9 @@ void AMyCPPPawn::Tick(float DeltaTime)
 }
 
 // Called to bind functionality to input
-void AMyCPPPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AMyCPPPawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
--- Source/Modelcule/MyCPP/MyCPPPawn.h
+++ Source/Modelcule/MyCPP/MyCPPPawn.h
@@ -4,22 +4,60 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Math/Rotator.h"
 #include "MyCPPPawn.generated.h"
+
 
 UCLASS()
 class MODELCULE_API AMyCPPPawn : public APawn
 {
 	GENERATED_BODY()
-
 public:
 	// Sets default values for this pawn's properties
 	AMyCPPPawn();
 
+	//Components
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* MainSceneComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere)
+	UArrowComponent* Arrow;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Collision;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* GrabLocation;
+
+
+	//Variables
 	UPROPERTY(Category = Default, BlueprintReadOnly)
-		bool IsGrabbing;
+	bool IsGrabbing;
 
 	UPROPERTY(Category=Default, BlueprintReadOnly)
-		bool IsOpenUI;
+	bool IsOpenUI;
+
+	//Fuctions
+	UFUNCTION(BlueprintCallable)
+	void GrabObject();
+
+	UFUNCTION(BlueprintCallable)
+	AActor* FindRootObj(AActor* currObj);
+
+	UFUNCTION(BlueprintCallable)
+		void RotateGrabbedObj(FRotator rot);
+
+
 
 protected:
 	// Called when the game starts or when spawned
